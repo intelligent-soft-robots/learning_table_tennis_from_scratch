ef77fcd24f10572a8954281ada51bdfd1489c017
Vincent Berenz <vberenz@tuebingen.mpg.de>
2021-03-25 16:53:07 +0100

    removed deprecated record to file mechanism

--- learning_table_tennis_from_scratch/hysr_one_ball_env.py
+++ learning_table_tennis_from_scratch/hysr_one_ball_env.py
@@ -70,8 +70,7 @@ class HysrOneBallEnv(gym.Env):
                                  rtt_cap=self._config.rtt_cap,
                                  trajectory_index=None,
                                  reference_posture=self._config.reference_posture,
-                                 pam_config=self._config.pam_config,
-                                 record_path=self._config.record_path)
+                                 pam_config=self._config.pam_config)
         
         self.action_space = gym.spaces.Box(low=-1.0,
                                            high=+1.0,
