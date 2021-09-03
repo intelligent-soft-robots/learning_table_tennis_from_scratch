a592fdde1d1015480b83a765ffdee6cbf1d0c80d
Vincent Berenz <vberenz@tuebingen.mpg.de>
2021-09-03 15:40:57 +0200

    replay of baselines ppo2 models working

--- learning_table_tennis_from_scratch/models.py
+++ learning_table_tennis_from_scratch/models.py
@@ -37,11 +37,12 @@ def run_stable_baselines(
 
 
 def run_openai_baselines(
-    reward_config_file,
-    hysr_one_ball_config_file,
-    ppo_config_file,
-    log_episodes=False,
-    log_tensorboard=False,
+        reward_config_file,
+        hysr_one_ball_config_file,
+        ppo_config_file,
+        log_episodes=False,
+        log_tensorboard=False,
+        model_file_path=None
 ):
 
     import baselines
@@ -76,18 +77,61 @@ def run_openai_baselines(
 
     alg = "ppo2"
     learn = get_alg_module_openai_baselines(alg).learn
+    
     # seed = 123
-    print("total timesteps:", total_timesteps)
-    model = learn(
-        env=env,
-        # seed=seed,
-        total_timesteps=total_timesteps,
-        **ppo_config
-    )
-    model.save("ppo2_openai_baselines_hysr_one_ball")
+    if model_file_path is None:
+        print("total timesteps:", total_timesteps)
+        model = learn(
+            env=env,
+            # seed=seed,
+            total_timesteps=total_timesteps,
+            **ppo_config
+        )
+        model.save("ppo2_openai_baselines_hysr_one_ball")
+
+    else:
+        ppo_config["load_path"]=model_file_path
+        model = learn(
+            env=env,
+            # seed=seed,
+            total_timesteps=0,
+            **ppo_config
+        )
+    
     return model, env
 
 
+def replay_openai_baselines(
+        model_file_path,
+        nb_episodes,
+        reward_config_file,
+        hysr_one_ball_config_file,
+        ppo_config_file,
+        log_episodes=False,
+        log_tensorboard=False,
+):
+
+    model,env = run_openai_baselines(
+        reward_config_file,
+        hysr_one_ball_config_file,
+        ppo_config_file,
+        log_episodes=False,
+        log_tensorboard=False,
+        model_file_path=model_file_path
+    )
+
+    observation = env.reset()
+    
+    for episode in range(nb_episodes):
+        done = False
+        while not done:
+            actions = model.step(observation)[0][0]
+            observation,_,done,__ = env.step([actions])
+
+    env.close()
+    
+                                             
+                                             
 def get_alg_module_openai_baselines(alg, submodule=None):
     from importlib import import_module
 
