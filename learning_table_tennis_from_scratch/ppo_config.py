0fc9021c11f6eef7721005ab9b710538640fc25b
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-04-05 11:23:22 +0200

    Migrate to stable_baselines3
    Migrate from stable_baselines 2 to stable_baselines3
    
    Apart from some adaptions in `run_stable_baselines()` the following
    changes were necessary:
    - Move parameter `log_tensorboard` from ppo_common_config.json to
      openai_ppo_default.json (sb3 uses "tensorboard_log" instead, which
      expects a path).
    - Rename parameters in ppo_default.json and add some parameters that
      were missing (num_timesteps, network size, etc.).
    - Pass logger as argument to `HysrOneBallEnv`.  This is necessary as
      openai baselines and sb3 are using different loggers.  For openai
      baselines use a wrapper around the logger to mimic the interface of
      the sb3 logger.

---
+++ learning_table_tennis_from_scratch/ppo_config.py
@@ -0,0 +1,162 @@
+import json
+import os
+import sys
+import typing
+
+
+ConfigDict = typing.Dict[str, typing.Any]
+
+
+class PPOConfig:
+    """Configuration for stable_baselines3 PPO."""
+
+    # These parameters are directly passed to stable_baselines3.PPO, see the
+    # documentation there for their meaning.
+    _ppo_params = (
+        "gamma",
+        "n_steps",
+        "ent_coef",
+        "learning_rate",
+        "clip_range",
+        "clip_range_vf",
+        "vf_coef",
+        "max_grad_norm",
+        "gae_lambda",
+        "batch_size",
+        "n_epochs",
+        "tensorboard_log",
+    )
+
+    # Additional parameters
+    _additional_params = (
+        "num_timesteps",  # 'total_timesteps' passed to PPO.learn()
+        "save_path",  # If set the model is saved to the given path.
+        "num_layers",  # Number of layers in the network
+        "num_hidden",  # Size of each layer (all layers have same size)
+    )
+
+    __slots__ = _ppo_params + _additional_params
+
+    def __init__(self):
+        for s in self.__slots__:
+            setattr(self, s, None)
+
+    def get(self) -> ConfigDict:
+        """Get dictionary with all parameters."""
+        return {attr: getattr(self, attr) for attr in self.__slots__}
+
+    def get_ppo_params(self) -> ConfigDict:
+        """Get dictionary with only the direct PPO parameters.
+
+        This includes only the parameters that can be passed as keyword arguments to
+        stable_baselines3.PPO.
+        """
+        return {attr: getattr(self, attr) for attr in self._ppo_params}
+
+    @classmethod
+    def from_dict(cls, d: ConfigDict) -> "PPOConfig":
+        instance = cls()
+        for s in instance.__slots__:
+            setattr(instance, s, d[s])
+        return instance
+
+    @classmethod
+    def from_json(cls, jsonpath: typing.Union[str, os.PathLike]) -> "PPOConfig":
+        if not os.path.isfile(jsonpath):
+            raise FileNotFoundError(
+                "failed to find PPO configuration file: {}".format(jsonpath)
+            )
+        try:
+            with open(jsonpath) as f:
+                conf = json.load(f)
+        except Exception as e:
+            raise ValueError(
+                "failed to parse PPO json configuration file {}: {}".format(jsonpath, e)
+            )
+        instance = cls()
+        for s in cls.__slots__:
+            try:
+                setattr(instance, s, conf[s])
+            except Exception:
+                raise ValueError(
+                    "failed to find the attribute {} " "in {}".format(s, jsonpath)
+                )
+        return instance
+
+    # FIXME Is this used anywhere?
+    @staticmethod
+    def default_path():
+        return os.path.join(
+            sys.prefix, "learning_table_tennis_from_scratch_config", "ppo_default.json"
+        )
+
+
+class OpenAIPPOConfig:
+    __slots__ = (
+        "gamma",
+        "num_timesteps",
+        "ent_coef",
+        "lr",
+        "cliprange",
+        "vf_coef",
+        "max_grad_norm",
+        "lam",
+        "nminibatches",
+        "noptepochs",
+        "network",
+        "num_layers",
+        "num_hidden",
+        "activation",
+        "nsteps",
+        "save_path",
+        "load_path",
+        "log_interval",
+        "save_interval",
+        "log_tensorboard",
+    )  # , "seed")
+
+    def __init__(self):
+        for s in self.__slots__:
+            setattr(self, s, None)
+
+    def get(self):
+        return {attr: getattr(self, attr) for attr in self.__slots__}
+
+    @classmethod
+    def from_dict(cls, d):
+        instance = cls()
+        for s in instance.__slots__:
+            setattr(instance, s, d[s])
+        return instance
+
+    @classmethod
+    def from_json(cls, jsonpath):
+        if not os.path.isfile(jsonpath):
+            raise FileNotFoundError(
+                "failed to find reward configuration file: {}".format(jsonpath)
+            )
+        try:
+            with open(jsonpath) as f:
+                conf = json.load(f)
+        except Exception as e:
+            raise ValueError(
+                "failed to parse reward json configuration file {}: {}".format(
+                    jsonpath, e
+                )
+            )
+        instance = cls()
+        for s in cls.__slots__:
+            try:
+                setattr(instance, s, conf[s])
+            except Exception:
+                raise ValueError(
+                    "failed to find the attribute {} " "in {}".format(s, jsonpath)
+                )
+        return instance.get()
+
+    @staticmethod
+    def default_path():
+        return os.path.join(
+            "/home/sguist/Workspaces_local/isr/learning_table_tennis_from_scratch/config",
+            "openai_ppo_default.json",
+        )
