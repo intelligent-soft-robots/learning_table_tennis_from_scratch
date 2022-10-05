645175908192eb260d780f54af24cf7d42aca721
Simon Guist <sguist@almond.is.localnet>
2022-10-05 12:21:16 +0200

    add ball velocity filtering for recorded trajectories

--- learning_table_tennis_from_scratch/hysr_one_ball.py
+++ learning_table_tennis_from_scratch/hysr_one_ball.py
@@ -98,6 +98,7 @@ class HysrOneBallConfig:
         "extra_balls_sets",
         "extra_balls_per_set",
         "trajectory_group",
+        "vel_filter_window_size",
         "frequency_monitoring_step",
         "frequency_monitoring_episode",
         "robot_integrity_check",
@@ -627,7 +628,7 @@ class HysrOneBall:
         # "load" the ball means creating the o80 commands corresponding
         # to the ball behavior (set by the "set_ball_behavior" method)
         trajectory = self._ball_behavior.get_trajectory()
-        iterator = context.ball_trajectories.BallTrajectories.iterate(trajectory)
+        iterator = context.ball_trajectories.BallTrajectories.iterate(trajectory, vel_filter_window_size = self._hysr_config.vel_filter_window_size)
         # setting the ball to the first trajectory point
         duration, state = next(iterator)
         self._ball_communication.set(state.get_position(), [0, 0, 0])
@@ -664,7 +665,7 @@ class HysrOneBall:
         for index_ball, (ball, trajectory) in enumerate(
             zip(self._extra_balls, trajectories)
         ):
-            iterator = context.ball_trajectories.BallTrajectories.iterate(trajectory)
+            iterator = context.ball_trajectories.BallTrajectories.iterate(trajectory, vel_filter_window_size = self._hysr_config.vel_filter_window_size)
             # going to first trajectory point
             _, state = next(iterator)
             item3d.set_position(state.get_position())
@@ -673,6 +674,7 @@ class HysrOneBall:
             # loading full trajectory
             for duration, state in iterator:
                 item3d.set_position(state.get_position())
+                print(index_ball, ":", state.get_position(), state.get_velocity())
                 item3d.set_velocity(state.get_velocity())
                 ball.frontend.add_command(
                     index_ball,
@@ -682,6 +684,7 @@ class HysrOneBall:
                 )
         for frontend in _ExtraBall.frontends.values():
             frontend.pulse()
+        input()
 
     def load_ball(self):
         # loading ball: setting all trajectories points
