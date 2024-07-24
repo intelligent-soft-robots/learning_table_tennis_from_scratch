b94ade57f6591f080fe6a9aa6101799d00471572
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2024-07-24 11:40:20 +0200

    Testing switch to o80 iterations

--- bin/debug_ball_recording2.py
+++ bin/debug_ball_recording2.py
@@ -22,9 +22,6 @@ if __name__ == '__main__':
     nb_sim_bursts = int(
         hysr_config.algo_time_step / hysr_config.mujoco_time_step
     )
-    frequency_manager = o80.FrequencyManager(
-        1.0 / hysr_config.algo_time_step
-    )
     ball_communication = simulated_robot_handle.interfaces[SEGMENT_ID_BALL]
     #ball_frontend = o80_pam.BallFrontEnd("ball")
     #ball_communication = o80_pam.o80Ball(ball_frontend)
@@ -35,9 +32,9 @@ if __name__ == '__main__':
     trajectory_orig = ball_behavior.get_trajectory()
 
     fake_recording_timestep = 10000
-    fake_timesteps = np.arange(0, 150 * fake_recording_timestep, fake_recording_timestep)
-    fake_positions = np.repeat(np.repeat(np.arange(30), 5)[:, None], 3, axis=1) / 30
-    trajectory_orig = (fake_timesteps, fake_positions)
+    # fake_timesteps = np.arange(0, 150 * fake_recording_timestep, fake_recording_timestep)
+    # fake_positions = np.repeat(np.repeat(np.arange(30), 5)[:, None], 3, axis=1) / 30
+    # trajectory_orig = (fake_timesteps, fake_positions)
 
     iterator = context.ball_trajectories.BallTrajectories.iterate(trajectory_orig)
 
@@ -57,27 +54,47 @@ if __name__ == '__main__':
     simulated_robot_handle.reset_contact(SEGMENT_ID_BALL)
     simulated_robot_handle.activate_contact(SEGMENT_ID_BALL)
     trajectory = []
+    trajectory_desired = []
     timestamps = []
     other_timestamps = []
     start = time.time()
     ts0 = None
+    previous_t = None
+    previous_iteration = None
+    frequency_manager = o80.FrequencyManager(
+        1.0 / hysr_config.algo_time_step
+    )
+    print("\n---------------------\n")
     for _ in range(150):
-        ts, pos, vel = ball_communication.get()
+        iteration, freq, ts, pos, vel, pos_desired, vel_desired = ball_communication.get()
         ts = ts*1e-3
         if ts0 is None:
             ts0 = ts
         parallel_burst.burst(nb_sim_bursts)
         trajectory.append(pos)
+        trajectory_desired.append(pos_desired)
         other_timestamps.append(ts-ts0)
-        timestamps.append((time.time() - start) * 1e6)
+        tnow = time.time()
+        if previous_t is None:
+            previous_t = tnow
+        if previous_iteration is None:
+            previous_iteration = iteration
+        timestamps.append((tnow - start) * 1e6)
         #print(time.time())
-        print(frequency_manager.wait())
+        #print(frequency_manager.wait(),tnow-previous_t, iteration-previous_iteration)
+        frequency_manager.wait()
+        #print(iteration,"\t",pos[2])
+        previous_t = tnow
+        previous_iteration = iteration
+
     #timestamps = np.array(timestamps)
     trajectory = np.array(trajectory)
+    trajectory_desired = np.array(trajectory_desired)
 
     #plt.plot(timestamps, timestamps)
     #plt.plot(timestamps,other_timestamps)
     plt.plot(timestamps, trajectory[:, 2], label="o80")
+    plt.plot(timestamps, trajectory_desired[:, 2], label="o80-desired")
     plt.plot(other_timestamps, trajectory[:, 2], label="o80_mujoco_timestamp")
     plt.plot(trajectory_orig[0], trajectory_orig[1][:, 2], label="file")
 
