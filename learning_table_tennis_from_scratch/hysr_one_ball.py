6ee2e14be410d665fcc286b215987b06548e17d6
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2024-10-28 11:05:19 +0100

    Fixed the pam_vicon import

--- learning_table_tennis_from_scratch/hysr_one_ball.py
+++ learning_table_tennis_from_scratch/hysr_one_ball.py
@@ -16,7 +16,6 @@ import o80
 import o80_pam
 import pam_interface
 import pam_mujoco
-#import pam_vicon_o80
 import context
 import frequency_monitoring
 import shared_memory
@@ -113,7 +112,7 @@ class HysrOneBallConfig:
     use_vicon: bool = False
     """If true, get robot and table pose from the Vicon system.
 
-    This requires a pam_vicon_o80 back end to be running, which provides the Vicon data.
+    This requires a pam_vicon back end to be running, which provides the Vicon data.
     The segment id used for this can be set via :attr:`vicon_segment_id`.
 
     If enabled, the values of :attr:`robot_position`, :attr:`robot_orientation`,
@@ -153,9 +152,10 @@ class HysrOneBallConfig:
 
         if cfg.use_vicon:
             try:
+                import pam_vicon
                 # get Vicon data via o80 (requires back end to be running in separate
                 # process)
-                vicon = pam_vicon_o80.PamVicon(cfg.vicon_segment_id)
+                vicon = pam_vicon.PamVicon(cfg.vicon_segment_id)
                 vicon.update()
                 robot_pose = vicon.get_robot_pose()
                 table_pose = vicon.get_table_pose(yaw_only=True)
