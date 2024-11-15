13fc3fbbf157ce4d234ebc37a316f72d131833fa
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2024-11-15 15:24:28 +0100

    Added support for gym environments, added check for nan values

--- bin/collect_tcr_intervention_scales.py
+++ bin/collect_tcr_intervention_scales.py
@@ -13,6 +13,8 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.utils import set_random_seed
 
+from tcr_data_collection import make_env
+
 logger = logging.getLogger(__name__)
 
 from learning_table_tennis_from_scratch.hysr_one_ball_env import HysrOneBallEnv
@@ -21,11 +23,8 @@ from learning_table_tennis_from_scratch.rl_config import RLConfig
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("train_logs", type=Path)
-    parser.add_argument(
-        "--config",
-        type=Path,
-        default=Path(__file__).parents[1] / "example" / "config.json",
-    )
+    parser.add_argument("--env-config", type=Path)
+    parser.add_argument("--env-name", type=str)
     parser.add_argument("--seed", type=int)
     parser.add_argument("--num-interventions", type=int, default=50)
     parser.add_argument("--max-intervention-std", type=float, default=1.0)
@@ -36,36 +35,31 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    with args.config.open() as f:
-        config = json.load(f)
+    if args.env_config is not None:
+        env_config = args.env_config
+    elif args.env_name is not None:
+        env_config = args.env_name
+    else:
+        raise ValueError("Either --env-config or --env-name must be specified")
 
-    algorithm = "ppo"
 
     if args.seed is not None:
         set_random_seed(args.seed)
 
-    rl_config = RLConfig.from_json(config["rl_config"], algorithm)
+    vec_env = make_env(env_config)
 
-    env_config = {
-        "reward_config_file": config["reward_config"],
-        "hysr_one_ball_config_file": config["hysr_config"],
-        "log_episodes": False,
-        "logger": None,
-    }
-    vec_env = make_vec_env(HysrOneBallEnv, env_kwargs=env_config, seed=args.seed)
     checkpoints_dir = args.train_logs / "checkpoints"
     checkpoint_path = sorted(
         [p for p in checkpoints_dir.iterdir()],
         key=lambda p: int(re.search("_\d+_", p.name).group()[1:-1]),
     )[-1]
 
-    # NOTE: It's important to set the seed when loading the model.  Otherwise the
+    # NOTE: It's important to set the seed when loading the model. Otherwise, the
     # RNG state will be restored from the loaded model, resulting in all runs to
     # behave the same.
     agent = PPO.load(checkpoint_path, vec_env, seed=args.seed, device="cpu")
 
     env = vec_env.envs[0]
-    num_ball_trajectories = env.unwrapped._hysr._ball_behavior._trajectory_reader.size()
     print("Starting data collection")
     rewards_int_stds = {}
     dist_target_int_stds = {}
@@ -73,35 +67,38 @@ if __name__ == "__main__":
     for intervention_std in tqdm.tqdm(intervention_stds):
         rewards_int_stds[intervention_std] = []
         dist_target_int_stds[intervention_std] = []
-        # Make sure that a different ball trajectory is used in every episode
-        ball_trajectory_indices = np.random.choice(
-            np.arange(num_ball_trajectories),
-            size=args.num_episodes_per_intervention,
-            replace=False,
-        )
-        for ball_traj_idx in ball_trajectory_indices:
-            try:
-                intervention_trajectory = np.random.normal(
-                    scale=intervention_std, size=(args.max_episode_length, 8)
-                )
-
+        if isinstance(env.unwrapped, HysrOneBallEnv):
+            num_ball_trajectories = env.unwrapped._hysr._ball_behavior._trajectory_reader.size()
+            ball_trajectory_indices = np.random.choice(
+                np.arange(num_ball_trajectories),
+                size=args.num_episodes_per_intervention,
+                replace=False,
+            )
+        for i in range(args.num_episodes_per_intervention):
+            if isinstance(env.unwrapped, HysrOneBallEnv):
                 env.unwrapped._hysr._ball_behavior.type = (
                     env.unwrapped._hysr._ball_behavior.INDEX
                 )
-                env.unwrapped._hysr._ball_behavior.value = ball_traj_idx
-                obs = env.reset()
+                env.unwrapped._hysr._ball_behavior.value = ball_trajectory_indices[i]
+
+            intervention_trajectory = np.random.normal(
+                scale=intervention_std, size=(args.max_episode_length, 8)
+            )
+            try:
+                obs, _ = env.reset()
                 rewards = []
                 for k in itertools.count():
                     action, _ = agent.predict(obs, deterministic=True)
-                    obs, reward, done, _ = env.step(action + intervention_trajectory[k])
+                    obs, reward, terminated, truncated, _ = env.step(action + intervention_trajectory[k])
+                    done = terminated or truncated
                     rewards.append(reward)
                     if done:
                         break
                 rewards_int_stds[intervention_std].append(np.sum(rewards))
-                dist_target_int_stds[intervention_std].append(env.unwrapped._hysr._ball_status.min_distance_ball_target)
+                if isinstance(env.unwrapped, HysrOneBallEnv):
+                    dist_target_int_stds[intervention_std].append(env.unwrapped._hysr._ball_status.min_distance_ball_target)
             except Exception:
-                print(f"Encountered exception for intervention std: {intervention_std}, "
-                      f"ball trajectory index: {ball_traj_idx}: \n{traceback.format_exc()}")
+                print(f"Encountered exception for intervention std: {intervention_std}: \n{traceback.format_exc()}")
 
         with args.outfile.open("wb") as outfile:
             pickle.dump({"rewards": rewards_int_stds, "distances_to_target": dist_target_int_stds}, outfile)
--- bin/tcr_data_collection.py
+++ bin/tcr_data_collection.py
@@ -5,38 +5,71 @@ import pickle
 import re
 import traceback
 from pathlib import Path
+from typing import Optional, Union
 
+import gymnasium as gym
 import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.vec_env import DummyVecEnv
 from tqdm import trange
 
 from learning_table_tennis_from_scratch.hysr_one_ball_env import HysrOneBallEnv
-from learning_table_tennis_from_scratch.rl_config import RLConfig
+
 
 def get_outpath(outdir: Path, job_id: str, intervention: int) -> Path:
     job_str = f"_job{job_id}" if job_id != "" else ""
     return outdir / f"data{job_str}_intervention{intervention}.pkl"
 
+
+def make_env(
+    env_config: Union[Path, str],
+    job_id: str = "",
+    seed: Optional[int] = None,
+    **gym_kwargs,
+):
+    if isinstance(env_config, Path):
+        with env_config.open() as f:
+            config = json.load(f)
+
+        dict_config = {
+            "reward_config_file": config["reward_config"],
+            "hysr_one_ball_config_file": config["hysr_config"],
+            "log_episodes": False,
+            "logger": None,
+            "job_id": job_id,
+        }
+        return make_vec_env(HysrOneBallEnv, env_kwargs=dict_config, seed=seed)
+    else:
+        vec_env = DummyVecEnv([lambda: gym.make(env_config, **gym_kwargs)])
+        if seed is not None:
+            vec_env.seed(seed)
+        return vec_env
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("train_logs", type=Path)
-    parser.add_argument(
-        "--config",
-        type=Path,
-        default=Path(__file__).parents[1] / "example" / "config.json",
-    )
+    parser.add_argument("--env-config", type=Path)
+    parser.add_argument("--env-name", type=str)
     parser.add_argument("--seed", type=int)
     parser.add_argument("--num-interventions", type=int, default=100)
     parser.add_argument("--num-episodes-per-intervention", type=int, default=20)
     parser.add_argument("--intervention-std", type=float, default=0.01)
-    parser.add_argument("--max-episode-length", type=int, default=200)
+    parser.add_argument("--max-episode-length", type=int)
     parser.add_argument(
         "--outdir", type=Path, default=Path(__file__).parents[1] / "out" / "tcr_dataset"
     )
     parser.add_argument("--job-id", type=str, default="")
     args = parser.parse_args()
+
+    if args.env_config is not None:
+        env_config = args.env_config
+    elif args.env_name is not None:
+        env_config = args.env_name
+    else:
+        raise ValueError("Either --env-config or --env-name must be specified")
 
     if not args.outdir.exists():
         args.outdir.mkdir(parents=True)
@@ -47,82 +80,108 @@ if __name__ == "__main__":
             if not get_outpath(args.outdir, args.job_id, start_intervention).exists():
                 break
 
-    with args.config.open() as f:
-        config = json.load(f)
-
-    algorithm = "ppo"
-
     if args.seed is not None:
         set_random_seed(args.seed)
 
-    rl_config = RLConfig.from_json(config["rl_config"], algorithm)
-
-    env_config = {
-        "reward_config_file": config["reward_config"],
-        "hysr_one_ball_config_file": config["hysr_config"],
-        "log_episodes": False,
-        "logger": None,
-        "job_id": args.job_id,
-    }
-    vec_env = make_vec_env(HysrOneBallEnv, env_kwargs=env_config, seed=args.seed)
+    vec_env = make_env(env_config, args.job_id)
     checkpoints_dir = args.train_logs / "checkpoints"
     checkpoint_path = sorted(
         [p for p in checkpoints_dir.iterdir()],
         key=lambda p: int(re.search("_\d+_", p.name).group()[1:-1]),
     )[-1]
 
-    # NOTE: It's important to set the seed when loading the model.  Otherwise the
+    # NOTE: It's important to set the seed when loading the model.  Otherwise, the
     # RNG state will be restored from the loaded model, resulting in all runs to
     # behave the same.
     agent = PPO.load(checkpoint_path, vec_env, seed=args.seed, device="cpu")
 
     env = vec_env.envs[0]
-    num_ball_trajectories = env.unwrapped._hysr._ball_behavior._trajectory_reader.size()
     print("Starting data collection")
     avg_reward = 0.0
-    for i in trange(start_intervention, args.num_interventions, initial=start_intervention, total=args.num_interventions):
+    for i in trange(
+        start_intervention,
+        args.num_interventions,
+        initial=start_intervention,
+        total=args.num_interventions,
+    ):
+        max_episode_length = (
+            args.max_episode_length
+            if args.max_episode_length is not None
+            else env.get_wrapper_attr("_max_episode_steps")
+        )
         intervention_trajectory = np.random.normal(
-            scale=args.intervention_std, size=(args.max_episode_length, 8)
+            scale=args.intervention_std, size=(max_episode_length, 8)
         )
         observation_trajectories = []
         action_trajectories = []
         reward_trajectories = []
         # Make sure that a different ball trajectory is used in every episode (otherwise multiple episodes would just be
         # the same...)
-        ball_trajectory_indices = np.random.choice(
-            np.arange(num_ball_trajectories),
-            size=args.num_episodes_per_intervention,
-            replace=False,
-        )
+        if isinstance(env.unwrapped, HysrOneBallEnv):
+            num_ball_trajectories = (
+                env.unwrapped._hysr._ball_behavior._trajectory_reader.size()
+            )
+            ball_trajectory_indices = np.random.choice(
+                np.arange(num_ball_trajectories),
+                size=args.num_episodes_per_intervention,
+                replace=False,
+            )
         j = 0
-        while j < len(ball_trajectory_indices):
-            ball_traj_idx = ball_trajectory_indices[j]
-            try:
+        while j < args.num_episodes_per_intervention:
+            observations = []
+            actions = []
+            rewards = []
+            if isinstance(env.unwrapped, HysrOneBallEnv):
+                ball_traj_idx = ball_trajectory_indices[j]
                 env.unwrapped._hysr._ball_behavior.type = (
                     env.unwrapped._hysr._ball_behavior.INDEX
                 )
                 env.unwrapped._hysr._ball_behavior.value = ball_traj_idx
-                obs = env.reset()
-                observations = [obs]
-                actions = []
-                rewards = []
+            try:
+                obs, _ = env.reset()
+                observations.append(obs)
                 for k in itertools.count():
                     action, _ = agent.predict(obs, deterministic=True)
-                    obs, reward, done, _ = env.step(action + intervention_trajectory[k])
+                    obs, reward, terminated, truncated, _ = env.step(
+                        action + intervention_trajectory[k]
+                    )
+                    done = terminated or truncated
                     observations.append(obs)
                     actions.append(action)
                     rewards.append(reward)
                     if done:
                         break
-                observation_trajectories.append(np.stack(observations, axis=0))
-                action_trajectories.append(np.stack(actions, axis=0))
-                reward_trajectories.append(np.stack(rewards, axis=0))
-                j += 1
+                observations_np = np.array(observations)
+                actions_np = np.array(actions)
+                rewards_np = np.array(rewards)
+                if (
+                    not np.any(np.isnan(observations_np))
+                    and not np.any(np.isnan(actions_np))
+                    and not np.any(np.isnan(rewards_np))
+                ):
+                    observation_trajectories.append(observations_np)
+                    action_trajectories.append(actions_np)
+                    reward_trajectories.append(rewards_np)
+                    j += 1
+                else:
+                    nan_value_field_names = [
+                        field_name
+                        for field_name, field_values in zip(
+                            ["observations", "actions", "rewards"],
+                            [observations_np, actions_np, rewards_np],
+                        )
+                        if np.any(np.isnan(field_values))
+                    ]
+                    print(f"Encountered NaN values for index {j} ({', '.join(nan_value_field_names)}). Retrying...")
             except:
-                print(f"Encountered exception for ball trajectory index: {ball_traj_idx}: \n{traceback.format_exc()}. "
-                      f"Retrying...")
+                print(
+                    f"Encountered exception for index {j}: \n{traceback.format_exc()}. Retrying..."
+                )
+
         num_rec_int = i - start_intervention
-        avg_reward = avg_reward * num_rec_int / (num_rec_int + 1) + np.mean([np.sum(r) for r in reward_trajectories]) / (num_rec_int + 1)
+        avg_reward = avg_reward * num_rec_int / (num_rec_int + 1) + np.mean(
+            [np.sum(r) for r in reward_trajectories]
+        ) / (num_rec_int + 1)
         print(f"Average reward: {avg_reward}")
         outpath = get_outpath(args.outdir, args.job_id, i)
         with outpath.open("wb") as outfile:
