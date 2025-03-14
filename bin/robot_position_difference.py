923b794070cb23e18bf33206c19e71870f771faa
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2025-03-14 19:10:30 +0100

    Updated expected position

--- bin/robot_position_difference.py
+++ bin/robot_position_difference.py
@@ -2,8 +2,8 @@ import numpy as np
 import pam_vicon.o80
 from scipy.spatial.transform import Rotation
 
-expected_position = np.array([0.23481061, -3.38250561, -0.95063758])
-expected_orientation = np.array([-0.02615020044781693, 0.021959215361716973, -0.008822280253092582, 0.999377870101381])
+expected_position = np.array([0.24172509, -3.38384842, -0.95054469])
+expected_orientation = np.array([-0.03136496, 0.02863854, -0.0100406, 0.99904718])
 expected_euler = Rotation.from_quat(expected_orientation).as_euler("xyz", degrees=True)
 
 vicon_frontend = pam_vicon.o80.FrontEnd("vicon")
@@ -15,4 +15,4 @@ while True:
     robot_orientation = robot_base.global_pose.get_rotation()
     robot_euler = Rotation.from_quat(robot_orientation).as_euler("xyz", degrees=True)
     with np.printoptions(precision=2, suppress=True):
-        print(f"Position diff.: {robot_position - expected_position}, Orientation diff.: {robot_euler - expected_euler}°", end="\r")
+        print(f"Position diff.: {robot_position - expected_position}, Orientation diff.: {robot_euler - expected_euler}°")
