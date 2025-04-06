48f6c1061779e3f686258abe21160d7f6ef8ea61
Simon Guist <sguist@almond.is.localnet>
2025-04-06 15:23:43 +0200

    use gymnasium instad of gym in remaining learning_table_tennis_from_scratch code

--- learning_table_tennis_from_scratch/hysr_goal_env.py
+++ learning_table_tennis_from_scratch/hysr_goal_env.py
@@ -4,7 +4,7 @@ import time
 from typing import Dict, Union
 from collections import OrderedDict
 
-import gymmnasium as gym
+import gymnasium as gym
 import gym_robotics
 import numpy as np
 import o80
