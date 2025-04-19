730e09f9886678b061276b1945f023707ad41c14
Simon Guist <sguist@almond.is.localnet>
2025-04-19 14:21:51 +0200

    check if save_folder_traj is empty

--- learning_table_tennis_from_scratch/hysr_many_ball_env.py
+++ learning_table_tennis_from_scratch/hysr_many_ball_env.py
@@ -91,13 +91,13 @@ class HysrManyBallEnv(gym.Env):
     ):
         super().__init__()
 
-        self._log_episodes = log_episodes
+        self._log_episodes = log_episodes and self._save_folder_traj!=""
         self._logger = logger
 
         hysr_one_ball_config = HysrOneBallConfig.from_json(hysr_one_ball_config_file)
 
         self._save_folder_traj = hysr_one_ball_config.save_folder_traj
-        if not os.path.exists(self._save_folder_traj):
+        if self._save_folder_traj and not os.path.exists(self._save_folder_traj):
             os.makedirs(self._save_folder_traj)
 
         reward_function = JsonReward.get(reward_config_file)
